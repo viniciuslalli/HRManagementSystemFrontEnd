9f3e9840137992d487989ac5044804b346532ce8
vlalli <viniciuslalli@gmail.com>
2024-02-14 22:01:18 -0300

    first commit

---
+++ CRUD/frondend/src/app/components/product/product.service.ts
@@ -0,0 +1,74 @@
+import { catchError, map } from "rxjs/operators";
+import { HttpClient } from "@angular/common/http";
+import { ThrowStmt } from "@angular/compiler";
+import { Injectable } from "@angular/core";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { EMPTY, Observable } from "rxjs";
+import { Product } from "./product.model";
+
+@Injectable({
+  providedIn: "root", // singleton, uma classe que tem apenas uma instância
+})
+export class ProductService {
+  baseUrl = "http://localhost:3001/products";
+
+  constructor(private snackBar: MatSnackBar, private http: HttpClient) {}
+
+  showMessage(msg: string, isError: boolean = false): void {
+    this.snackBar.open(msg, "x", {
+      duration: 3000,
+      horizontalPosition: "right",
+      verticalPosition: "top",
+      panelClass: isError ? ["msg-error"] : ["msg-sucess"],
+    });
+  }
+
+  create(product: Product): Observable<Product> {
+    return this.http.post<Product>(this.baseUrl, product).pipe(
+      //pipe para retornar um observable// caso cair em algum erro no tach ele vai cair no error
+      map((obj) => obj),
+      catchError((e) => this.errorHandler(e))
+    ); // faz um post em nosso banco de dados
+  }
+
+  errorHandler(e: any): Observable<any> {
+    console.log(e);
+    this.showMessage("Ocorreu um erro!", true);
+    return EMPTY;
+  }
+
+  read(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.baseUrl).pipe(
+      //pipe para retornar um observable// caso cair em algum erro no tach ele vai cair no error
+      map((obj) => obj),
+      catchError((e) => this.errorHandler(e))
+    ); // ;
+  }
+
+  readById(id: string): Observable<Product> {
+    const url = `${this.baseUrl}/${id}`;
+    return this.http.get<Product>(url).pipe(
+      //pipe para retornar um observable// caso cair em algum erro no tach ele vai cair no error
+      map((obj) => obj),
+      catchError((e) => this.errorHandler(e))
+    ); // ;
+  }
+
+  update(product: Product): Observable<Product> {
+    const url = `${this.baseUrl}/${product.id}`;
+    return this.http.put<Product>(url, product).pipe(
+      //pipe para retornar um observable// caso cair em algum erro no tach ele vai cair no error
+      map((obj) => obj),
+      catchError((e) => this.errorHandler(e))
+    ); // ;
+  }
+
+  delete(id: number | undefined): Observable<Product> {
+    const url = `${this.baseUrl}/${id}`;
+    return this.http.delete<Product>(url).pipe(
+      //pipe para retornar um observable// caso cair em algum erro no tach ele vai cair no error
+      map((obj) => obj),
+      catchError((e) => this.errorHandler(e))
+    ); // ;
+  }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# Angular-CRUD
+
+First things first: 
+
+- npm install into frontend and backend folder
+- npm start first in backend folder to start the "server" 
+- npm start in frontend folder to start the application
+
+That's it! Enjoy this short application to solidify your knowledge of Angular
